c701d6ed874d848b192b9481f003a2f40cb771a7
KrishR-Cedcoss <krishnamrastogi@cedcoss>
2022-08-23 12:10:08 +0530

    TAsk Done

--- src/App.js
+++ src/App.js
@@ -87,6 +87,77 @@ function App() {
     </Fragment>
   );
 
+  useEffect(()=>{
+
+    let changeTheme = location.current.condition.text.toLowerCase();
+    if (changeTheme.indexOf("sun") > -1) {
+      theme =
+        "https://imengine.public.prod.cmg.infomaker.io/?uuid=c1d3deeb-6e98-5e7e-a0c2-a3d30517b8e7&function=cropresize&type=preview&source=false&q=75&crop_w=0.99999&crop_h=0.99999&width=1200&height=675&x=1.0E-5&y=1.0E-5";
+      document.body.style.background = `url(${theme})`;
+      document.body.style.backgroundRepeat = "no-repeat";
+      document.body.style.backgroundSize = "cover";
+      console.log('sun');
+    }
+     else if (
+      changeTheme.indexOf("cloud") > -1 ||
+      changeTheme.indexOf("mist") > -1 ||
+      changeTheme.indexOf("overcast") > -1
+    ) {
+      theme =
+        "https://t3.ftcdn.net/jpg/03/02/03/70/360_F_302037028_WgdzBqp7MCTF0iITajUUVryCKJsyjOE6.jpg";
+      document.body.style.background = `url(${theme})`;
+      document.body.style.backgroundRepeat = "no-repeat";
+      document.body.style.backgroundSize = "cover";
+      
+      console.log('cloud');
+    }
+     else if (
+      changeTheme.indexOf("rain") > -1 ||
+      changeTheme.indexOf("drizz") > -1
+    ) {
+      theme =
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoNLDZk--fZbmveHr5eViMMPkpfDm-P50ck5m9CoU3TMDFndoVHk16LRR2Bv_eqPVVjNc&usqp=CAU";
+      document.body.style.background = `url(${theme})`;
+      document.getElementById("weather").style.color = "white";
+      document.body.style.backgroundRepeat = "no-repeat";
+      document.body.style.backgroundSize = "cover";
+      console.log("rain");
+    }
+     else if (
+      changeTheme.indexOf("freez") > -1 ||
+      changeTheme.indexOf("snow") > -1 ||
+      changeTheme.indexOf("sleet") > -1 ||
+      changeTheme.indexOf("blizzard") > -1
+    ) {
+      theme =
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzqi3IScIzuHifT85lrRc3uUKDdAWfa5Lesg&usqp=CAU";
+      document.body.style.background = `url(${theme})`;
+      document.getElementById("weather").style.color = "black";
+      document.body.style.backgroundRepeat = "no-repeat";
+      document.body.style.backgroundSize = "cover";
+      console.log('snow');
+    }
+     else if (
+      changeTheme.indexOf("fog") > -1
+    ) {
+      theme =
+        "https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/weather/fog--mist/fog-on-a-country-road.jpg";
+      document.body.style.background = `url(${theme})`;
+      document.body.style.backgroundRepeat = "no-repeat";
+      document.body.style.backgroundSize = "cover";
+      console.log('fog');
+    }
+     else {
+      theme =
+        "https://img.freepik.com/free-vector/gorgeous-clouds-background-with-blue-sky-design_1017-25501.jpg";
+      document.body.style.background = `url(${theme})`;
+      document.body.style.backgroundRepeat = "no-repeat";
+      document.body.style.backgroundSize = "cover";
+      console.log('default');
+    }
+  },[location])
+
+
   //function to perform search for places
   const searchLoc = (event) => {
     if (event.key !== "Enter") {
@@ -95,6 +166,7 @@ function App() {
       document.getElementById("error").innerHTML = "";
       let locationName = event.target.value;
 
+      
       axios(
         `https://api.weatherapi.com/v1/current.json?key=0bab7dd1bacc418689b143833220304&q=${locationName.toLowerCase()}`
       )
@@ -106,66 +178,6 @@ function App() {
           handleClick();
         });
 
-      if (location.current.condition.text.toLowerCase().indexOf("sun") > -1) {
-        theme =
-          "https://imengine.public.prod.cmg.infomaker.io/?uuid=c1d3deeb-6e98-5e7e-a0c2-a3d30517b8e7&function=cropresize&type=preview&source=false&q=75&crop_w=0.99999&crop_h=0.99999&width=1200&height=675&x=1.0E-5&y=1.0E-5";
-        document.body.style.background = `url(${theme})`;
-        document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.backgroundSize = "cover";
-        setCount((count) => count + 1);
-      } else if (
-        location.current.condition.text.toLowerCase().indexOf("cloud") > -1 ||
-        location.current.condition.text.toLowerCase().indexOf("mist") > -1 ||
-        location.current.condition.text.toLowerCase().indexOf("overcast") > -1
-      ) {
-        theme =
-          "https://t3.ftcdn.net/jpg/03/02/03/70/360_F_302037028_WgdzBqp7MCTF0iITajUUVryCKJsyjOE6.jpg";
-        document.body.style.background = `url(${theme})`;
-        document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.backgroundSize = "cover";
-        setCount((count) => count + 1);
-        console.log(count);
-      } else if (
-        location.current.condition.text.toLowerCase().indexOf("rain") > -1 ||
-        location.current.condition.text.toLowerCase().indexOf("drizz") > -1
-      ) {
-        theme =
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoNLDZk--fZbmveHr5eViMMPkpfDm-P50ck5m9CoU3TMDFndoVHk16LRR2Bv_eqPVVjNc&usqp=CAU";
-        document.body.style.background = `url(${theme})`;
-        document.getElementById("weather").style.color = "white";
-        document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.backgroundSize = "cover";
-        setCount((count) => count + 1);
-      } else if (
-        location.current.condition.text.toLowerCase().indexOf("freez") > -1 ||
-        location.current.condition.text.toLowerCase().indexOf("snow") > -1 ||
-        location.current.condition.text.toLowerCase().indexOf("sleet") > -1 ||
-        location.current.condition.text.toLowerCase().indexOf("blizzard") > -1
-      ) {
-        theme =
-          "https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&poi=face&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F34%2F2020%2F12%2F18%2Fsnow-storm-blizzard-getty-1220-2000.jpg";
-        document.body.style.background = `url(${theme})`;
-        document.getElementById("weather").style.color = "black";
-        document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.backgroundSize = "cover";
-        setCount((count) => count + 1);
-      } else if (
-        location.current.condition.text.toLowerCase().indexOf("fog") > -1
-      ) {
-        theme =
-          "https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/weather/fog--mist/fog-on-a-country-road.jpg";
-        document.body.style.background = `url(${theme})`;
-        document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.backgroundSize = "cover";
-        setCount((count) => count + 1);
-      } else {
-        theme =
-          "https://img.freepik.com/free-vector/gorgeous-clouds-background-with-blue-sky-design_1017-25501.jpg";
-        document.body.style.background = `url(${theme})`;
-        document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.backgroundSize = "cover";
-        setCount((count) => count + 1);
-      }
     }
   };
 
